8b4ba7dc9b4813040a0b43a0f38356280cc386e9
arbaz-ops <arbazachughtai55@gmail.com>
2021-09-10 11:37:16 +0500

    first commit

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    var center: CLLocationCoordinate2D?
+    var span: MKCoordinateSpan?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let myLocation = locations.first else {return}
+        center = CLLocationCoordinate2D(latitude: myLocation.coordinate.latitude, longitude: myLocation.coordinate.longitude)
+        span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        mapView.region = MKCoordinateRegion(center: center!, span: span!)
+    }
 }
 
